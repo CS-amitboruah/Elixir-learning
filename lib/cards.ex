da0c626cca3416974da0a8fd697780ff62edd676
CS-amitboruah <amit.boruah@creolestudios.com>
2022-12-28 17:46:55 +0530

    learn Documentation and testing

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,11 @@
 defmodule Cards do
-  # def hello do
-  #   "hello world"
-  # end
+  @moduledoc """
+   Provides methods to shuffle the deck hand the desire number of random cards.
+  """
+
+  def hello do
+    "hello world"
+  end
 
   # def create_deck do
   #   values = ['Ace', 'One', 'Two', 'Three', 'Four', 'Five', 'Six']
@@ -17,8 +21,11 @@ defmodule Cards do
   #   List.flatten(cards)
   # end
 
+  @doc """
+  combine the number and the face of the cards, and create the deck from it.
+  """
   def create_deck do
-    values = ['Ace', 'One', 'Two', 'Three', 'Four', 'Five', 'Six']
+    values = ['Ace', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
     suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
 
     cards =
@@ -29,14 +36,35 @@ defmodule Cards do
     List.flatten(cards)
   end
 
+  @doc """
+  this method will take deck as a parameter and shuffle that deck.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Determines whether the mention card present inside the deck or not
+
+  ## Examples
+      iex>deck = Cards.create_deck
+      iex>Cards.contains?(deck,"Ace of Hearts ")
+      iex>true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  this method will take a deck ask for the `hand_size` , and hand the number of card from the deck of the cards.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex>{hand,deck} = Cards.deal(deck,1)
+      iex>hand
+      ["Ace of Spades "]
+  """
   def deal(deck, hand_Size) do
     Enum.split(deck, hand_Size)
   end
@@ -46,12 +74,22 @@ defmodule Cards do
     File.write(filename, binary)
   end
 
-  def load(filename) do
-    {status, binary} = File.read(filename)
+  # def load(filename) do
+  #   {status, binary} = File.read(filename)
 
-    case status do
-      {:ok, binary} -> :erlang.binary_to_term(binary)
-      {:error, reason} -> "File does not exist"
-    end
+  #   case status do
+  #     {:ok, _binary} -> :erlang.binary_to_term(binary)
+  #     {:error, _reason} -> "File does not exist"
+  #   end
+  # end
+
+  def create_hand(hand_size) do
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck,hand_size)
+
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "Check the number of cards created" do
+    deck_lenght = length(Cards.create_deck())
+    assert deck_lenght = 36
+  end
+
+  test "check if the deck get shuffled or not" do
+    deck = Cards.create_deck()
+    # assert deck != Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
   end
 end
